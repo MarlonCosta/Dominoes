10fbdda4ad2589f4a735934eb06a79c8e1f59c74
MarlonCosta <mrlnrnn@gmail.com>
2022-03-14 23:01:54 -0300

    Stage 2 completed, adding the interface for the game

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -3,7 +3,7 @@ import random
 stock_pcs = []
 player_pieces = []
 comp_pieces = []
-starting_player = None
+status = None
 starting_double = None
 
 
@@ -32,47 +32,49 @@ class DominoPiece:
             return self.tail
 
 
-def get_starting_piece():
+def get_starting_player_and_piece():
+    global player_pieces, status, comp_pieces
     max_double_number = -1
     max_double_piece = None
-    player = None
-    global player_pieces
-    global comp_pieces
 
+    #   Filtering doubles
     pp_doubles = [p for p in player_pieces if p.is_double()]
     cp_doubles = [p for p in comp_pieces if p.is_double()]
 
+    #   Reshufle if there is no doubles
     while pp_doubles == [] and cp_doubles == []:
         player_pieces, comp_pieces = distribute_pieces(stock_pcs)
 
+    #   Both for loops define the highest double piece on players possession
     for p in pp_doubles:
         if p.greater_side() > max_double_number:
             max_double_number = p.greater_side()
             max_double_piece = p
-            player = "computer"
+            status = "computer"
 
     for p in cp_doubles:
         if p.greater_side() > max_double_number:
             max_double_number = p.greater_side()
             max_double_piece = p
-            player = "player"
+            status = "player"
 
-    if player == "player":
+    # Removes the Domino snake from the player set
+    if status == "player":
         comp_pieces.remove(max_double_piece)
-    if player == "computer":
+    if status == "computer":
         player_pieces.remove(max_double_piece)
 
-    return player, max_double_piece
+    return status, max_double_piece
 
 
-def distribute_pieces(ss: list):
-    pp = []
-    cp = []
+def distribute_pieces(stock_set: list):
+    player_pcs = []
+    computer_pcs = []
     for i in range(7):
-        pp.append(ss.pop(random.randint(0, 27 - i)))
+        player_pcs.append(stock_set.pop(random.randint(0, 27 - i)))
     for j in range(7):
-        cp.append(ss.pop(random.randint(0, 20 - j)))
-    return pp, cp
+        computer_pcs.append(stock_set.pop(random.randint(0, 20 - j)))
+    return player_pcs, computer_pcs
 
 
 def generate_domino_set():
@@ -85,13 +87,22 @@ def generate_domino_set():
     return domino_set
 
 
+def print_interface():
+    print("======================================================================")
+    print(f"Stock size: {len(stock_pcs)}")
+    print(f"Computer pieces: {len(comp_pieces)}\n")
+    print(starting_double)
+    print("\nYour pieces:")
+    [print(f"{count}: {piece}") for count, piece in enumerate(player_pieces, 1)]
+
+    if status == "player":
+        print("Status: It's your turn to make a move. Enter your command.")
+    else:
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+
+
 if __name__ == '__main__':
     stock_pcs = generate_domino_set()
     player_pieces, comp_pieces = distribute_pieces(stock_pcs)
-    starting_player, starting_double = get_starting_piece()
-
-    print("Stock pieces:", stock_pcs)
-    print("Computer pieces:", comp_pieces)
-    print("Player pieces:", player_pieces)
-    print("Domino snake:", [starting_double])
-    print("Status:", starting_player)
+    status, starting_double = get_starting_player_and_piece()
+    print_interface()
